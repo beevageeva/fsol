a4ca8df213d60e8ed9a4accd5a504cd19ab01df1
Beatrice Popescu Braileanu <bpopescu@talla.ll.iac.es>
2016-06-07 15:40:28 +0100

    ge

--- part2/oc/int.m
+++ part2/oc/int.m
@@ -1,11 +1,16 @@
-f[x_]:= Sqrt[(1+Cos[x])/2 - ((1+Cos[x])/2)^2] + ArcCos[(1+Cos[x])/2]
+(* $Assumptions = {Element[{rs,R,x}, Reals],  rs>0 , R>0} *)
+
+f[x_]:= Sqrt[(1+Cos[x])/2 - ((1+Cos[x])/2)^2] + ArcCos[Sqrt[(1+Cos[x])/2]]
 Print[f[x]]
 g[x_]:= D[f[x],x]
 Print[g[x]]
 h[x_]:=1/ Sqrt[1-(2 rs)/ (R (1+Cos[x])) ] * g[x]
-Print[h[x]]
+Print[FullSimplify[h[x]] // TeXForm]
+(* l[x_]:=Integrate[FullSimplify[h[x]],x] *)
 l[x_]:=Integrate[h[x],x]
-Print[l[x]]
+ Print[FullSimplify[l[x]]] 
+
+Print[FullSimplify[l[x]] // TeXForm]
 
 
 
